b5e1fa0fe4a27da5c403b6b2c13f5438bee56398
fathergoose <al@ilseman.com>
2015-12-23 21:20:49 -0600

    precompile required plupoad

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/buttons.png )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/buttons-disabled.png )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/transp50.png )
+Rails.application.config.assets.precompile += %w( Moxie.swf )
+Rails.application.config.assets.precompile += %w( Moxie.xap )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/backgrounds.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/delete.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/error.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/done.gif )
