407696db32d1c84d182174abe196a0fb5484ec3b
fathergoose <al@ilseman.com>
2015-12-27 14:18:10 -0600

    fixed path + track creation + index

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,9 +1,9 @@
 class TracksController < ApplicationController
   def index
     if user_signed_in?
-      @tracks = Track.user_tracks
+      @tracks = Track.user_tracks(current_user)
     else
-      redirect_to 'pages#welcome'
+      redirect_to '/pages/welcome'
     end
   end
 
@@ -15,8 +15,9 @@ class TracksController < ApplicationController
   #  @track = Track.new(
   #    path: params["path"]
   #  )
-    @track = Track.new
-    @track.path = params[:file]
+    p current_user
+    @track = Track.new(user_id: current_user.id)
+    @track.path = params[:path]
     @track.save
     p @track
     @track.save_tags!(@track.id)
