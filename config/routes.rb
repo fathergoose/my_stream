a5ca56a8c775b3cdd3b864b74ef4fedf42b4a77d
fathergoose <al@ilseman.com>
2015-12-27 14:16:06 -0600

    routing welcome#index as fallback

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
 
   delete 'tracks/:id' => 'tracks#destroy'
 
+  ## Pages actions
+  
+  get 'pages/welcome' => 'pages#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
