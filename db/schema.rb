2fe350a0d98db856b2e4371333aa6cd6634b403c
fathergoose <al@ilseman.com>
2015-12-27 08:59:46 -0600

    added user id to tracks + migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227053053) do
+ActiveRecord::Schema.define(version: 20151227145704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151227053053) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "path"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
