7377e7a237f3d049c2a526c2590ab141307fc225
fathergoose <al@ilseman.com>
2016-01-08 12:27:46 -0600

    playlists have names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108182000) do
+ActiveRecord::Schema.define(version: 20160108182525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160108182000) do
   create_table "playlists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "track_albums", force: :cascade do |t|
