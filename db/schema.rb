c50290d3bca160a85aaf45cb8a9f856049bba120
fathergoose <al@ilseman.com>
2016-01-08 12:21:18 -0600

    playlist is a model now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104021341) do
+ActiveRecord::Schema.define(version: 20160108182000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,17 +36,7 @@ ActiveRecord::Schema.define(version: 20160104021341) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "playlist_tracks", id: false, force: :cascade do |t|
-    t.integer  "queue_position"
-    t.integer  "playlist_id"
-    t.integer  "track_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "playlists", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
