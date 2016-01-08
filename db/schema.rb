61af64b1ea572f45d05aaf0b25bb58d7e2448115
fathergoose <al@ilseman.com>
2016-01-08 12:32:07 -0600

    PlaylistTrack is a model w/ migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108182525) do
+ActiveRecord::Schema.define(version: 20160108183002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20160108182525) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "playlist_tracks", force: :cascade do |t|
+    t.integer  "playlist_id"
+    t.integer  "track_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "playlists", force: :cascade do |t|
